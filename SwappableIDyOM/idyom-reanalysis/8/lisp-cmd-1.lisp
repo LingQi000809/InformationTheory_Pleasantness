a77ee4a3ae7c8094607d9611c8df2c8436c02109
LingQi000809 <lqi60@gatech.edu>
2026-06-01 20:03:09 -0400

    Add IDyOM swap experiment

---
+++ SwappableIDyOM/idyom-reanalysis/8/lisp-cmd-1.lisp
@@ -0,0 +1,23 @@
+
+                        (progn 
+                        (defvar *idyom-root* "/Users/peter/idyom/")
+                        (defvar *idyom-code-root*
+                        (pathname "/Users/peter/quicklisp/local-projects/idyom/"))
+                        (defvar *idyom-message-detail-level* 2)
+                        (ql:quickload "idyom")
+                        (clsql:connect '("/Users/peter/idyom/db/database.sqlite") :if-exists :old :database-type :sqlite3)
+                        (eval (read-from-string "
+                        (progn 
+                        (idyom-db:delete-dataset 888888)
+                        (idyom-db:delete-dataset 999999)
+                        (idyom-db:import-data :txt \"/Users/peter/Dropbox/Academic/projects/entropy-vs-surprisal/output/stimuli/May-25/using-stim-corr-and-picking-best-sequences/idyom-reanalysis/8/new-training-data.txt\" \"Temporary Billboard training set\" 888888)
+                        (idyom-db:import-data :txt \"/Users/peter/Dropbox/Academic/projects/entropy-vs-surprisal/output/stimuli/May-25/using-stim-corr-and-picking-best-sequences/idyom-reanalysis/8/new-test-data.txt\" \"Temporary Billboard test set\" 999999)
+                        (utils:message \"Recomputing information-theoretic estimates...\")
+                        (let ((viewpoints::*basic-types* '(:cpitch)))
+                        (idyom:idyom 999999 '(cpitch) '(cpitch) :k 1
+                        :models :both
+                        :pretraining-ids '(888888)
+                        :output-path \"/Users/peter/Dropbox/Academic/projects/entropy-vs-surprisal/output/stimuli/May-25/using-stim-corr-and-picking-best-sequences/idyom-reanalysis/8/recomputed-idyom-output\"
+                        :use-resampling-set-cache? nil
+                        :use-ltms-cache? nil)))"))
+                        (sb-ext:quit))
